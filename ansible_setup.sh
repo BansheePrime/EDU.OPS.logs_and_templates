4135f56f2dfa212408222b7b338c8e7b6797901d
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-06-07 13:06:57 +0300

    adding ansible installation from ppa script

---
+++ ansible_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+apt update
+apt install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt update
+# Ansible bash completion support
+apt install python3-argcomplete
+activate-global-python-argcomplete3
+# Check
+echo $(ansible --version)
