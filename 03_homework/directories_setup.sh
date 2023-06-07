4a88ad27b69dd288d607eb4bef95593fb980b2e8
gidrevichan <gidrevichan@mosmetod.ru>
2023-06-07 10:01:00 +0300

    fixing [] in setup script

--- 03_homework/directories_setup.sh
+++ 03_homework/directories_setup.sh
@@ -3,7 +3,7 @@ mkdir -p /var/www/cookie-research/html/
 cp -rf ./attacker1.html /var/www/cookie-research/html/
 cp -rf ./subattacker2.html /var/www/cookie-research/html/
 cp -rf ./subsubattacker3.html /var/www/cookie-research/html/
-chmod -r 755 /var/www/cookie-research/
+chmod -R 755 /var/www/cookie-research/
 
 # Update Nginx conf
 cp -rf ./cookie-research.conf /etc/nginx/sites-available/
@@ -19,11 +19,11 @@ else
     echo "Error: Directory /var/www/cookie-research/ does not exists."
 fi
 # Test if html files set
-if [ -f /var/www/cookie-research/html/subsubattacker3.html -a -f /var/www/cookie-research/html/subattacker2.html -a -f /var/www/cookie-research/html/attacker1.html]; then
+if [[ -f /var/www/cookie-research/html/subsubattacker3.html -a -f /var/www/cookie-research/html/subattacker2.html -a -f /var/www/cookie-research/html/attacker1.html]]; then
     echo "DONE: Files set as wanted."
 fi
 # Test if nginx conf updated
-if [ -f /etc/nginx/sites-available/cookie-research.conf]; then
+if [[ -f /etc/nginx/sites-available/cookie-research.conf]]; then
     echo "DONE: Nginx conf updated."
 fi
 echo " "
