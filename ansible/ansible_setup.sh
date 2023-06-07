8c5ee3c3298a6c99d750f4b149ce584ab069fdee
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-06-07 13:13:05 +0300

    shame on me

--- ansible/ansible_setup.sh
+++ ansible/ansible_setup.sh
@@ -3,6 +3,7 @@ apt update
 apt install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt update
+apt install ansible
 # Ansible bash completion support
 apt install python3-argcomplete
 activate-global-python-argcomplete3
