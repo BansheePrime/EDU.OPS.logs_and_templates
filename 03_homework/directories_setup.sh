54c75c23d1ea28a837ae5d764a2e3ccad238f24c
gidrevichan <gidrevichan@mosmetod.ru>
2023-06-07 10:09:55 +0300

    removing files set in script - bugging for NO reasons

--- 03_homework/directories_setup.sh
+++ 03_homework/directories_setup.sh
@@ -9,29 +9,6 @@ chmod -R 755 /var/www/cookie-research
 cp -rf ./cookie-research.conf /etc/nginx/sites-available/
 # Linking avail and enabled
 sudo ln -s /etc/nginx/sites-available/cookie-research.conf /etc/nginx/sites-enabled/
-
-# Tests
-# Test if directory exists
-if [ -d "/var/www/cookie-research/" ] 
-then
-    echo "DONE: Directory /var/www/cookie-research/ exists." 
-else
-    echo "Error: Directory /var/www/cookie-research/ does not exists."
-fi
-# Test if html files set
-if [ -f /var/www/cookie-research/html/subsubattacker3.html]; then
-    echo "DONE: File subsubattacker3.html set."
-fi
-if [ -f /var/www/cookie-research/html/subattacker2.html]; then
-    echo "DONE: File subattacker2.html set."
-fi
-if [ -f /var/www/cookie-research/html/attacker1.html]; then
-    echo "DONE: File attacker1.html set."
-fi
-# Test if nginx conf updated
-if [[ -f /etc/nginx/sites-available/cookie-research.conf]]; then
-    echo "DONE: Nginx conf updated."
-fi
 echo " "
 echo "Do not forget to restart Nginx with: sudo systemctl restart nginx"
 echo " "
